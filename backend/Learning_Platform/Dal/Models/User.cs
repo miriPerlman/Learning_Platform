041eef1195edcc5da256e41a0d5d6de190e86559
miriPerlman <miri3210839@gmail.com>
2025-06-09 13:42:40 +0300

    Initial commit: Set up project structure

---
+++ backend/Learning_Platform/Dal/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dal.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+}
